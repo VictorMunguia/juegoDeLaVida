8563a5d619814c5d78c804d617987a7cb6602031
Victor Munguia <victor00171@hotmail.com>
2017-12-18 02:25:37 -0600

    Metodo de comparacion de casillas vivas y muertas junto a un metodo de comparacionde estado

--- js.js
+++ js.js
@@ -45,3 +45,45 @@ function cambiarTamano(){
 	else
 		alert("Dimensiones fuera de los límites");
 }
+
+//Compara el entorno de una célula con los parametros dados y retorna la suma de la cantidad de células vivas a su alrededor
+function compararEntorno(i,j)
+{
+	let vida=0;
+	if(i>=1 )//Arriba
+		if(document.getElementById((i-1)+","+j).className == "viva")
+			vida++;
+	if(i<largo-1)//Abajo
+		if(document.getElementById((i+1)+","+j).className == "viva")
+			vida++;
+	if(j<largo-1)//Derecha
+		if(document.getElementById(i+","+(j+1)).className == "viva")
+			vida++;
+	if(j>=1)//Izquierda
+		if(document.getElementById(i+","+(j-1)).className == "viva")
+			vida++;
+	if(i>=1 && j<largo-1)//Arriba derecha
+		if(document.getElementById((i-1)+","+(j+1)).className == "viva")
+			vida++;
+	if(i>=1 && j!=0)//Arriba izquierda
+		if(document.getElementById((i-1)+","+(j-1)).className == "viva")
+			vida++;
+	if(i<largo-1 && j<largo-1)//Abajo derecha
+		if(document.getElementById((i+1)+","+(j+1)).className == "viva")
+			vida++;
+	if(i<largo-1 && j>0)//Abajo izquierda
+		if(document.getElementById((i+1)+","+(j-1)).className == "viva")
+			vida++;
+	return vida;
+}
+
+//A partir del resultado que retorna la función compararEntorno se decide si la célula vive, renace, muere o permanece en el estado en el que se encontraba anteriormente
+function evaluarEstado(i,j){
+	let resultadoComparacion=compararEntorno(i,j);
+	if(document.getElementById(i+","+j).className=="muerta" && resultadoComparacion==3)
+		estado[i][j]="viva";
+	else if(document.getElementById(i+","+j).className=="viva" && (resultadoComparacion==2 || resultadoComparacion==3))
+		estado[i][j]="viva";
+	else
+		estado[i][j]="muerta";	
+}
